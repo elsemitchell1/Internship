8a2a73f0b3f47822a45e9d5589998a03b31bba73
Mitchell Else <elsemitchell1@gmail.com>
2022-01-19 21:44:52 -0400

    Updates

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export {default as Navbar} from './Navbar/Navbar';
-export {default as InfoSection} from './InfoSection/InfoSection';
-export {default as Footer} from './Footer/Footer';
-export {default as Rover} from './Rover/Rover';
-export {default as Hazard} from './Hazard/Hazard';
